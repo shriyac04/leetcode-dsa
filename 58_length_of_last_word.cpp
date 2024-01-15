972a1fa9f46b55f7169b58c985448fa9553da3ed
shriyac04 <122461216+shriyac04@users.noreply.github.com>
2024-01-15 20:35:53 +0530

    length of the last word

---
+++ 58_length_of_last_word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(std::string s) {
+        int siz = s.length();
+        int count = 0;
+
+        // Iterate from the end of the string to find the last word
+        for (int i = siz - 1; i >= 0; i--) {
+            if (s[i] != ' ') {
+                count++;
+            } else if (count > 0) {
+                // If a space is encountered after the last word, break the loop
+                break;
+            }
+        }
+
+        return count;
+    }
+};
