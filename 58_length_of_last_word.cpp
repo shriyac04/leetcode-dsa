5ff1ef3208b5fbe1648c6c5aa024ed795d20328b
shriyac04 <122461216+shriyac04@users.noreply.github.com>
2024-01-22 16:42:30 +0530

    length of the last word

--- 58_length_of_last_word.cpp
+++ 58_length_of_last_word.cpp
@@ -17,3 +17,20 @@ public:
         return count;
     }
 };
+
+APPROACH-2:
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int siz=s.size(),counts=0,flag=0;
+        for(int i=siz-1;i>=0;i--){
+            if(s[i]==' '&&flag)break;
+            if(s[i]!=' '){
+                flag=1;
+                counts++;
+            }
+        }
+        return counts;
+    }
+};
